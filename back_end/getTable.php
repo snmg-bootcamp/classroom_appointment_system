6e6de4275ba6fecc4825b592a1b7f11ba24ff447
KaiBro <w181496@gmail.com>
2014-11-09 20:18:10 +0800

    fix wrong arguments display error

--- back_end/getTable.php
+++ back_end/getTable.php
@@ -15,11 +15,20 @@
 	if(isset($_GET['class']))
 		$class = $_GET['class'];
 
-	$url = 'http://classroom.csie.ncu.edu.tw/appointment_schedule/'.$class;
-
-	echo "<br><br><br>";
-
-	echo getUrlContent($resource, $url);
-	//echo json_encode(filter(getUrlContent($resource, $url)), JSON_UNESCAPED_UNICODE);
-
+	$flag = 0;
+	for($i = 0; $i < 13; $i++) {
+		if($ClassList[$i] == $class) {
+			$flag = 1;
+			break;
+		}
+	}
+	if($flag) {
+		$url = 'http://classroom.csie.ncu.edu.tw/appointment_schedule/'.$class;
+		echo "<br><br><br>";
+		//echo getUrlContent($resource, $url);
+		echo json_encode(filter(getUrlContent($resource, $url)), JSON_UNESCAPED_UNICODE);
+	}
+	else {
+		echo "查無此教室資料";
+	}
 ?>
