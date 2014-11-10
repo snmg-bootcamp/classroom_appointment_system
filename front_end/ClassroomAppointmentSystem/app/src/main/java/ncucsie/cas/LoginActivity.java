937c48dab2de7a55b7c8393f3b80ef8af284af7b
petercommand <petercommand@gmail.com>
2014-11-10 13:29:20 +0800

    swtich activity on successful login

--- front_end/ClassroomAppointmentSystem/app/src/main/java/ncucsie/cas/LoginActivity.java
+++ front_end/ClassroomAppointmentSystem/app/src/main/java/ncucsie/cas/LoginActivity.java
@@ -5,7 +5,9 @@ import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.LoaderManager.LoaderCallbacks;
+import android.content.Context;
 import android.content.CursorLoader;
+import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
@@ -280,6 +282,8 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
             showProgress(false);
 
             if (success) {
+                Intent intent = new Intent(LoginActivity.this, MainActivityDrawer.class);
+                startActivity(intent);
                 finish();
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
