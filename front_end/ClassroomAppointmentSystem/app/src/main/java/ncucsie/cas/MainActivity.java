a6718a1d826d63dac5317f4d2a7230ab2e98d0fd
petercommand <petercommand@gmail.com>
2014-11-10 19:12:08 +0800

    remove unused import

--- front_end/ClassroomAppointmentSystem/app/src/main/java/ncucsie/cas/MainActivity.java
+++ front_end/ClassroomAppointmentSystem/app/src/main/java/ncucsie/cas/MainActivity.java
@@ -1,16 +1,8 @@
 package ncucsie.cas;
 
 import android.app.Activity;
-import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentTabHost;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 
 public class MainActivity extends Activity {
 
