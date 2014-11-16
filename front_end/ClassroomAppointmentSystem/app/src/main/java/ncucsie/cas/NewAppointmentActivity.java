e1b6cbc6d3e97034fabc46fe634626ac50ffcc01
petercommand <petercommand@gmail.com>
2014-11-16 21:41:10 +0800

    set show custom action bar

--- front_end/ClassroomAppointmentSystem/app/src/main/java/ncucsie/cas/NewAppointmentActivity.java
+++ front_end/ClassroomAppointmentSystem/app/src/main/java/ncucsie/cas/NewAppointmentActivity.java
@@ -14,12 +14,14 @@ public class NewAppointmentActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_appointment_tab);
+
         View actionBarButtons = getLayoutInflater().inflate(R.layout.custom_form_custom_actionbar,
                 new LinearLayout(this), false);
         View cancelActionView = actionBarButtons.findViewById(R.id.action_cancel);
         //cancelActionView.setOnClickListener(mActionBarListener);
         View doneActionView = actionBarButtons.findViewById(R.id.action_done);
         //doneActionView.setOnClickListener(mActionBarListener);
+        getActionBar().setDisplayShowCustomEnabled(true);
         getActionBar().setCustomView(actionBarButtons);
     }
 
