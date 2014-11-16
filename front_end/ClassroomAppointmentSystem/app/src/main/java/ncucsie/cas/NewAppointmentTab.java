037953b0337c7619a0f70953a4be2446455e4470
petercommand <petercommand@gmail.com>
2014-11-16 21:13:16 +0800

    add plcaeholder listeners for actionbar items

--- front_end/ClassroomAppointmentSystem/app/src/main/java/ncucsie/cas/NewAppointmentTab.java
+++ front_end/ClassroomAppointmentSystem/app/src/main/java/ncucsie/cas/NewAppointmentTab.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 
 public class NewAppointmentTab extends Fragment {
@@ -23,7 +24,13 @@ public class NewAppointmentTab extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.new_appointment_tab, container, false);
-
+        View actionBarButtons = inflater.inflate(R.layout.custom_form_custom_actionbar,
+                new LinearLayout(getActivity()), false);
+        View cancelActionView = actionBarButtons.findViewById(R.id.action_cancel);
+        //cancelActionView.setOnClickListener(mActionBarListener);
+        View doneActionView = actionBarButtons.findViewById(R.id.action_done);
+        //doneActionView.setOnClickListener(mActionBarListener);
+        getActivity().getActionBar().setCustomView(actionBarButtons);
         setRetainInstance(true);
         return rootView;
     }
