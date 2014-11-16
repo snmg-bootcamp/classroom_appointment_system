62f911ff031065ab74bd13d85d8e27749bd3a888
petercommand <petercommand@gmail.com>
2014-11-16 21:36:55 +0800

    remove redundant imports

--- front_end/ClassroomAppointmentSystem/app/src/main/java/ncucsie/cas/NewAppointmentActivity.java
+++ front_end/ClassroomAppointmentSystem/app/src/main/java/ncucsie/cas/NewAppointmentActivity.java
@@ -1,12 +1,8 @@
 package ncucsie.cas;
 
 import android.app.Activity;
-import android.app.Fragment;
-import android.content.Context;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
 
