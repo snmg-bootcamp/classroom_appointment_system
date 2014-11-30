5dc692d5c8c8c39d9a0f1d22e0a6765577faa5d6
KaiBro <w181496@gmail.com>
2014-11-30 23:42:57 +0800

    update getAllTable

--- back_end/getAllTable.php
+++ back_end/getAllTable.php
@@ -4,20 +4,49 @@
 
 	include('config.php');
 	include('func.php');
-		
-	//login and set cookie
-	$url = 'http://classroom.csie.ncu.edu.tw/appointment_rule?destination=appointment_rule';
-	$postdata = "name=$User&pass=$Pass&form_id=user_login_block";
-	$resource = setUrlCookie($url, $postdata);
-	$total = array();
-	for($i = 0; $i < 13; $i++) {
-		$url = 'http://classroom.csie.ncu.edu.tw/appointment_schedule/'.$ClassList[$i];
-		$total[$ClassList[$i]] = filter(getUrlContent($resource, $url));
+	
+	$status         = "400";    // default status code (error)
+	$last_modified  = NULL;
+	$last_modified  = NULL;
+
+ 	//check the user whether send auth token and request data (json format)
+	if(isset($_POST['data'])) {
+		$data = json_decode($_POST['data']);
+
+		if(isset($data -> {'client_ver'}) &&
+		   isset($data -> {'classroom'}) &&
+		   isset($data -> {'appointment-date'}) &&
+		   isset($data -> {'sessionid'}) &&
+		   isset($data -> {'last-modified'})
+		)
+		{
+			$client_ver        =  $data -> {'client_ver'};
+			$class             =  $data -> {'classroom'};
+			$appointment_date  =  $data -> {'appointment-date'};
+			$token             =  $data -> {'sessionid'};
+			$last_modified     =  $data -> {'last-modified'};
+
+			if($client_ver != $version) {
+				$status = 401;  // wrong client version, client need to be updated.
+	        }
+	        else {
+				$total = array();
+				for($i = 0; $i < 13; $i++) {
+					$url = 'http://classroom.csie.ncu.edu.tw/appointment_schedule/'.$ClassList[$i];
+					$total[$ClassList[$i]] = filter(getUrlContent($url, $token));
+				}
+				/*
+				print_r($total);
+				echo "<br><br>";
+				*/
+				$response = $total;
+				$status = 200;
+			}
+		}
 	}
-	/*
-	print_r($total);
-	echo "<br><br>";
-	*/
-	$total = json_encode($total, JSON_UNESCAPED_UNICODE);
-	echo $total;
+	$response_arr = array("status_code"    => $status,
+	                      "last_modified"  => $last_modified,
+	                      "response"       => $response
+	                     );
+	echo json_encode($response_arr, JSON_UNESCAPED_UNICODE);
 ?>
