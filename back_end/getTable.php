9cc51689255b08b309c15fdf8f57c1edd4aa65ea
KaiBro <w181496@gmail.com>
2014-11-30 14:10:56 +0800

    fix some bug

--- back_end/getTable.php
+++ back_end/getTable.php
@@ -10,31 +10,40 @@
 	//check the user whether send auth token and request data (json format)
 	if(isset($_POST['data'])) {
 		$data       	   =  json_decode($_POST['data']);
-		$client_ver 	   =  $data -> {'client_ver'};
-		$class             =  $data -> {'classroom'};
-		$appointment_date  =  $data -> {'appointment-date'};
-		$token			   =  $data -> {'sessionid'};
-		$last_modified	   =  $data -> {'last-modified'};
-
-		//login and set cookie
-		/*
-		$url = 'http://classroom.csie.ncu.edu.tw/appointment_rule?destination=appointment_rule';
-		$postdata = "name=$User&pass=$Pass&form_id=user_login_block";
-		$resource = setUrlCookie($url, $postdata, $User);
-		*/
 		
-		$flag = 0;
-		for($i = 0; $i < 13; $i++) {
-			if($ClassList[$i] === $class) {
-				$flag = 1;
-				break;
+		if(isset($data -> {'client_ver'}) && 
+		   isset($data -> {'classroom'}) && 
+		   isset($data -> {'appointment-date'}) && 
+		   isset($data -> {'sessionid'}) &&
+		   isset($data -> {'last-modified'})
+		)
+		{
+			$client_ver 	   =  $data -> {'client_ver'};
+			$class             =  $data -> {'classroom'};
+			$appointment_date  =  $data -> {'appointment-date'};
+			$token			   =  $data -> {'sessionid'};
+			$last_modified	   =  $data -> {'last-modified'};
+
+			//login and set cookie
+			/*
+			   $url = 'http://classroom.csie.ncu.edu.tw/appointment_rule?destination=appointment_rule';
+			   $postdata = "name=$User&pass=$Pass&form_id=user_login_block";
+			   $resource = setUrlCookie($url, $postdata, $User);
+			 */
+
+			$flag = 0;
+			for($i = 0; $i < 13; $i++) {
+				if($ClassList[$i] === $class) {
+					$flag = 1;
+					break;
+				}
 			}
-		}
-		if($flag) {
-			$url = 'http://classroom.csie.ncu.edu.tw/appointment_schedule/'.$class;
-			//echo getUrlContent($resource, $url);
-			$status = 200;	// success
-			$response = filter(getUrlContent($url, $token));
+			if($flag) {
+				$url = 'http://classroom.csie.ncu.edu.tw/appointment_schedule/'.$class;
+				//echo getUrlContent($resource, $url);
+				$status = 200;	// success
+				$response = filter(getUrlContent($url, $token));
+			}	
 		}
 	}
 	$response_arr = array("status_code"    => $status, 
