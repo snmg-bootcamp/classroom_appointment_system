70500f138f44fbce63e60cbc6c74a89bd4baba12
petercommand <petercommand@gmail.com>
2014-12-03 13:44:04 +0800

    add functions to conduct basic server communication, close #15

--- front_end/ClassroomAppointmentSystem/app/src/main/java/ncucsie/cas/InternetComm.java
+++ front_end/ClassroomAppointmentSystem/app/src/main/java/ncucsie/cas/InternetComm.java
@@ -3,6 +3,18 @@ package ncucsie.cas;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class InternetComm {
     Context mContext;
@@ -17,4 +29,75 @@ public class InternetComm {
         return (networkInfo != null && networkInfo.isConnected());
     }
 
+    private class ApiRequest extends AsyncTask<String, Void, JSONObject> {
+        @Override
+        protected JSONObject doInBackground(String... urls) {
+
+            // params comes from the execute() call: params[0] is the url.
+            try {
+                return downloadUrl(urls[0], urls[1]);
+            } catch (IOException e) {
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(JSONObject result) {
+            //TODO: remove placeholder
+            //perform update action after successful update or when failed to update, notify the user immediately
+        }
+
+
+    }
+
+    // Given a URL, establishes an HttpUrlConnection and retrieves
+// the web page content as a InputStream, which it returns as
+// a string.
+    private JSONObject downloadUrl(String myurl, String postData) throws IOException {
+        InputStream is = null;
+        // Only display the first 500 characters of the retrieved
+        // web page content.
+        int len = 500;
+
+        try {
+            URL url = new URL(myurl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("POST");
+            conn.setDoInput(true);
+            // Starts the query
+            conn.connect();
+            int response = conn.getResponseCode();
+            Log.d(Constant.DEBUG_TAG, "The response is: " + response);
+            is = conn.getInputStream();
+            // Convert the InputStream into a string
+            try {
+                return InputStreamToJSONObject(is);
+            }
+            catch (Exception JSONException) {
+                return null;
+            }
+            // Makes sure that the InputStream is closed after the app is
+            // finished using it.
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+        }
+    }
+
+    private static JSONObject InputStreamToJSONObject(InputStream inputStream)
+            throws JSONException {
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        try {
+            while ((line = bufferedReader.readLine()) != null)
+                result += line;
+        } catch (Exception IOException){
+            Log.i(Constant.DEBUG_TAG, "IO exception in bufferedReader.readLine()");
+        }
+        return new JSONObject(result);
+    }
 }
