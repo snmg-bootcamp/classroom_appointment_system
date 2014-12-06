eb44c7fc0eeb6a0f18429c848fd8a0f3289ea44e
petercommand <petercommand@gmail.com>
2014-12-06 09:39:45 +0800

    add custom adapter class

--- front_end/ClassroomAppointmentSystem/app/src/main/java/ncucsie/cas/MyAppointmentTab.java
+++ front_end/ClassroomAppointmentSystem/app/src/main/java/ncucsie/cas/MyAppointmentTab.java
@@ -2,11 +2,15 @@ package ncucsie.cas;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
 
+import java.util.ArrayList;
 
 public class MyAppointmentTab extends Fragment {
     public MyAppointmentTab() {
