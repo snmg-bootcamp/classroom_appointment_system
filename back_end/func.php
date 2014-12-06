e1e348aa14dd5008524f180bede10a7e1dddd2bd
KaiBro <w181496@gmail.com>
2014-12-06 22:01:26 +0800

    add function 'CaculateWeekDay'

--- back_end/func.php
+++ back_end/func.php
@@ -181,4 +181,17 @@
 		return $arr;
 	}
 
+	function CaculateWeekDay($y, $m, $d)
+	{
+		if($m == 1 || $m == 2)
+		{
+			$m += 12;
+			$y--;
+		}
+		
+		$week = ($d + 2 * $m + 3 * ($m + 1) / 5 + $y + $y / 4 - $y / 100 + $y / 400) % 7;
+
+		return $week + 1;
+	}
+
 ?>
