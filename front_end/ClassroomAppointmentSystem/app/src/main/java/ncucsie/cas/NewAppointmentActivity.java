4781578e5b3a9a54c948c532cfaf7f90660f71e8
petercommand <petercommand@gmail.com>
2014-12-15 18:14:02 +0800

    fix forget to show toast

--- front_end/ClassroomAppointmentSystem/app/src/main/java/ncucsie/cas/NewAppointmentActivity.java
+++ front_end/ClassroomAppointmentSystem/app/src/main/java/ncucsie/cas/NewAppointmentActivity.java
@@ -36,7 +36,7 @@ public class NewAppointmentActivity extends Activity
                     this.onBackPressed();
                     break;
                 default:
-                    Toast.makeText(this, "Failed to create appointment", Toast.LENGTH_LONG);
+                    Toast.makeText(this, "Failed to create appointment, " + result.getString("response"), Toast.LENGTH_LONG).show();
                     break;
 
             }
